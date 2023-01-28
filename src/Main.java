2d8519ef2f9981c6c652a75d6244a72154d72533
georgegoldman <georgegoldmanjohn.o@gmail.com>
2023-01-28 16:58:01 +0100

    updt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 
 class Main {
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		String name = "Onyedikachi Georgegoldman John";
+		String country = "Nigeria";
+		int age = 26;
+		String company = "MUSALA";
+		double gpa = 3.8;
+		char percent = '%';
+		boolean amItellingTheTruth = true;
+		String formattedString = String.format("My name is %s.I am from %s. I am %d years old. I work at %s. My gps is %s, I have attended 60%c of my university classes. These are all %b claims", name, country, age, company, gpa, percent, amItellingTheTruth );
+		
+		System.out.println(formattedString);
 	}
 }
